4223b8730ce86cd8f39d3aae52b46cb7a653ab49
blessing-bester <blessingphir79@gmail.com>
2025-11-30 09:03:18 +0200

    compute module variables

---
+++ terraform/modules/compute/variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "public_subnet_id" {
+  description = "Public subnet ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
