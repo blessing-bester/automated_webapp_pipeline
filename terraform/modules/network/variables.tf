f5dce44d5e218ada3a6fcb646b1bc4dc24c12bd7
blessing-bester <blessingphir79@gmail.com>
2025-11-30 09:00:52 +0200

    network module variables

---
+++ terraform/modules/network/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "Public subnet CIDR"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
