a70c97cdc3863eb7179c67f6698f8c4e746c5610
blessing-bester <blessingphir79@gmail.com>
2025-11-30 08:55:55 +0200

    dev variables

---
+++ terraform/environments/dev/variables.tf
@@ -0,0 +1,41 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "Public subnet CIDR"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"  # Free tier eligible
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+  default     = "devops-project-key"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "devops-resume"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
